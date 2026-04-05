381255084b1725fc73dae24600dea01bcdb31a8a
thoratprashant <prashant.thorat@arkenea.com>
2026-04-06 00:19:53 +0530

    Forgot password completed

--- src/app/features/auth/forgot-password/forgot-password.ts
+++ src/app/features/auth/forgot-password/forgot-password.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,MatIconModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.scss',
 })
