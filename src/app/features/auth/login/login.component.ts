79f0434fa3589c538b6f645e6275e8d3fee1529d
thoratprashant <prashant.thorat@arkenea.com>
2026-04-05 23:40:22 +0530

    Login Completed

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -5,21 +5,39 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';  
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink,MatButtonModule, MatIconModule,MatCheckboxModule],
+  imports: [CommonModule, RouterLink,MatButtonModule, MatIconModule,MatCheckboxModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
  
-  showNewPassword = true;  
-  
-  toggleNewPassword(): void {
+  form!: FormGroup;
+  showNewPassword = false;   
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  toggleNewPassword() {
     this.showNewPassword = !this.showNewPassword;
   }
+
+submit() {
+  if (this.form.invalid) {
+    this.form.markAllAsTouched();
+    return;
+  }
+} 
 
   
  
