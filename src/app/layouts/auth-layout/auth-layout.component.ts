70f0c0cc68cc71c996699569f41c6f08c7f17209
thoratprashant <prashant.thorat@arkenea.com>
2026-04-06 22:55:03 +0530

    Completed Auth module

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-auth-layout',
@@ -8,6 +9,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
 })
-export class AuthLayoutComponent {
+export class AuthLayoutComponent implements OnInit{
+  isLoginPage: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkRoute(this.router.url);
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.checkRoute(event.urlAfterRedirects);
+      });
+  }
+
+  checkRoute(url: string) {
+    this.isLoginPage = url.includes('/auth/login');
+  }
 }
