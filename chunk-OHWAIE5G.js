f2ce981330223ded1901d089b27192a6aa5d1bd2
thoratprashant <prashant.thorat@arkenea.com>
2026-06-11 17:35:15 +0530

    Auto-generated commit

---
+++ chunk-OHWAIE5G.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var o=[{path:"",loadComponent:()=>import("./chunk-DZUNDTE3.js").then(t=>t.SystemError),data:{title:"system-error"}}];export{o as SYSTEMERRORLAYOUT_ROUTES};
