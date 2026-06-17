85b36256c0a21b60910a110e044e712fc441d5d7
thoratprashant <prashant.thorat@arkenea.com>
2026-06-17 13:14:13 +0530

    Auto-generated commit

---
+++ chunk-CAXHN7KR.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-XP4VG2U2.js").then(o=>o.Todo),data:{title:"todo"}}];export{t as TODO_ROUTES};
--- chunk-SDK5QJMJ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-JF4J33IC.js").then(o=>o.Todo),data:{title:"todo"}}];export{t as TODO_ROUTES};
