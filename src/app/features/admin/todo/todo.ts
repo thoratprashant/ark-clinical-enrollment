69a713f8bf67ada8c7c845dd84e3d2c40ced2580
thoratprashant <prashant.thorat@arkenea.com>
2026-06-18 07:58:37 +0530

    Added Congratution popup

--- src/app/features/admin/todo/todo.ts
+++ src/app/features/admin/todo/todo.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
 import AOS from 'aos';
 
@@ -13,30 +13,102 @@ interface PriorityTodo {
   initials: string;
   name: string;
   phone: string;
-  due: string; 
+  due: string;
 }
+
 interface TodoSlide {
   id: number;
   type: 'gold' | 'silver' | 'bronze';
-  userName: string; 
-  initials:string;
+  userName: string;
+  initials: string;
   medalimage: string;
   horizon: string;
-  contactno:string;
-  noteData:string;
-  hours:string;
-  minite:string;
-  leftdata:string;
-  pulsing:string;
+  contactno: string;
+  noteData: string;
+  hours: string;
+  minite: string;
+  leftdata: string;
+  pulsing: string;
 }
 
 @Component({
   selector: 'app-todo',
-  imports: [CommonModule,MatInputModule,FormsModule,MatFormFieldModule,MatSelectModule,MatIconModule,MatDatepickerModule, OwlDateTimeModule, OwlNativeDateTimeModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDatepickerModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
+  ],
   templateUrl: './todo.html',
   styleUrl: './todo.scss',
 })
-export class Todo {
+export class Todo implements OnInit, OnDestroy {
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  showCongratulationPopup = false;
+
+  private openTimer?: ReturnType<typeof setTimeout>;
+  private closeTimer?: ReturnType<typeof setTimeout>;
+
+  confettiItems = Array.from({ length: 120 }, (_, i) => {
+    const side = Math.random() > 0.5 ? 1 : -1;
+
+    return {
+      id: i,
+      x: (Math.random() * window.innerWidth - window.innerWidth / 2) * 1.8,
+      y: (Math.random() * window.innerHeight - window.innerHeight / 2) * 1.4,
+      rotate: Math.random() * 900 * side,
+      delay: Math.random() * 0.16,
+      size: 6 + Math.random() * 12
+    };
+  });
+
+  stripItems = Array.from({ length: 14 }, (_, i) => i);
+
+  ngOnInit(): void {
+    this.openTimer = setTimeout(() => {
+      this.openCongratulationPopup();
+    }, 2000);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.openTimer);
+    clearTimeout(this.closeTimer);
+  }
+
+  openCongratulationPopup(): void {
+    this.generateConfetti();
+    this.showCongratulationPopup = true;
+    this.cdr.detectChanges();
+
+    this.closeTimer = setTimeout(() => {
+      this.closeCongratulationPopup();
+    }, 3000);
+  }
+  closeCongratulationPopup(): void {
+    this.showCongratulationPopup = false;
+    this.cdr.detectChanges();
+  }
+
+  generateConfetti(): void {
+    this.confettiItems = Array.from({ length: 120 }, (_, i) => {
+      const side = Math.random() > 0.5 ? 1 : -1;
+
+      return {
+        id: i,
+        x: (Math.random() * window.innerWidth - window.innerWidth / 2) * 1.8,
+        y: (Math.random() * window.innerHeight - window.innerHeight / 2) * 1.4,
+        rotate: Math.random() * 900 * side,
+        delay: Math.random() * 0.16,
+        size: 6 + Math.random() * 12
+      };
+    });
+  }
 
   activeIndex = 0;
 
@@ -46,43 +118,43 @@ export class Todo {
       type: 'gold',
       userName: 'Emily Gold',
       initials: 'EG',
-      horizon:'HORIZON-1 Trial',
-      contactno:'+1 345 678 9012',
-      noteData:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit , Lorem ipsum dolor sit amet, consectetur adipiscing dolor sit amet, consectetur adipiscing it Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit', 
+      horizon: 'HORIZON-1 Trial',
+      contactno: '+1 345 678 9012',
+      noteData: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit , Lorem ipsum dolor sit amet, consectetur adipiscing dolor sit amet, consectetur adipiscing it Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit',
       medalimage: 'gold-medal.png',
-      hours:'1',
-      minite:'32',
-      leftdata:'left for GOLD',
-      pulsing:'0-2 hrs · Pulsing'
+      hours: '1',
+      minite: '32',
+      leftdata: 'left for GOLD',
+      pulsing: '0-2 hrs Â· Pulsing'
     },
     {
       id: 2,
       type: 'silver',
-      userName: 'Particia Silver', 
-      initials:'PS',
-      horizon:'HORIZON-2 Trial',
-      contactno:'+1 895 547 2154',
-      noteData:'Lorem ipsum dolor  it Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit , Lorem ipsum dolor sit amet, consectetur adipiscing dolor sit amet, consectetur adipiscing it Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit', 
+      userName: 'Particia Silver',
+      initials: 'PS',
+      horizon: 'HORIZON-2 Trial',
+      contactno: '+1 895 547 2154',
+      noteData: 'Lorem ipsum dolor  it Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit , Lorem ipsum dolor sit amet, consectetur adipiscing dolor sit amet, consectetur adipiscing it Lorem ipsum dolor sit amet, consectetur adipiscing eli Lorem ipsum dolor sit amet, consectetur adipiscing elit',
       medalimage: 'silver-medal.png',
-      hours:'2',
-      minite:'24',
-      leftdata:'left for SILVER',
-      pulsing:'0-3 hrs · Pulsing'
+      hours: '2',
+      minite: '24',
+      leftdata: 'left for SILVER',
+      pulsing: '0-3 hrs Â· Pulsing'
     },
     {
       id: 3,
       type: 'bronze',
       userName: 'Carter Bronze',
       initials: 'CB',
-      horizon:'HORIZON-3 Trial',
-      contactno:'+1 741 852 9632',
-      noteData:'Lorem ipsu i Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsu i Lorem ipsum dolor sit amet, consectetur adipiscing elit',  
+      horizon: 'HORIZON-3 Trial',
+      contactno: '+1 741 852 9632',
+      noteData: 'Lorem ipsu i Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsu i Lorem ipsum dolor sit amet, consectetur adipiscing elit',
       medalimage: 'bronze-medal.png',
-      hours:'3',
-      minite:'44',
-      leftdata:'left for BRONZE',
-      pulsing:'0-4 hrs · Pulsing'
-    }, 
+      hours: '3',
+      minite: '44',
+      leftdata: 'left for BRONZE',
+      pulsing: '0-4 hrs Â· Pulsing'
+    },
   ];
 
   get nextIndex(): number {
@@ -98,9 +170,6 @@ export class Todo {
     this.activeIndex =
       (this.activeIndex + 1) % this.slides.length;
   }
-
-
-
 
   todos: PriorityTodo[] = [
     {
@@ -133,7 +202,8 @@ export class Todo {
     AOS.init({
       duration: 1000,
       once: true
-    }); 
+    });
+
     AOS.refresh();
   }
 }
